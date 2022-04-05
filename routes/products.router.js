3aec97ca48dec7bdeba79e587d90c919c2bf7ecc
WJCruz <wilder.cruz.a@uni.pe>
2022-04-04 23:09:16 -0500

    la wea fome

---
+++ routes/products.router.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+const ProductService = require('./../services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemas/product.schema');
+
+const router = express.Router();
+const service = new ProductService();
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await service.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req,res, next) => { //se coloca ":id" para indicar que id es un parámetro
+    try {
+      //const id = req.params.id;
+      const { id } = req.params; //id tiene que llamarse igual que lo que está después de los dos puntos :
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error); //con esta sentencia se ejecuta los middlewares de tipo error
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body; //recibe todos los parámetros que llegan desde insomnia
+      const newProduct = await service.create(body);
+      res.status(201).json(newProduct); //colocamos el codigo de estatus en 201
+    } catch (error) {
+      next(error);
+    }
+});
+
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'), //validamos el id
+  validatorHandler(updateProductSchema, 'body'), //validamos los datos
+  async (req,res, next) => { //colocamos el id para identificar el producto que queremos editar
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+)
+
+router.delete('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
